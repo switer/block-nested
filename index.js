47f2ee6482bbd2cf915ffdd8d8197504aa64dc65
switer <guankaishe@gmail.com>
2015-08-24 11:49:04 +0800

    TBD

--- index.js
+++ index.js
@@ -1,21 +1,61 @@
 var text = "{a{1}a}"
 var tag = /\{|\}/g
+
+function handler(openTag, content, closeTag) {
+	return content
+}
+
+function isTag (c) {
+	return isOpenTag(c) || isCloseTag()
+}
 function isOpenTag (c) {
 	return c == '{'
 }
+function isCloseTag (c) {
+	return c == '}'
+}
+function isEmpty (stack) {
+	return !stack.length
+}
+function stackTop (stack) {
+	return stack[stack.length - 1]
+}
 
-var words = text.split(tag)
-var tokens = text.match(tag)
+function join(arr1, arr2) {
+	var len = arr1.length > arr2 ? arr1.length : arr2.length
+	var joinedArr = []
+	while(len --) {
+		joinedArr.push(arr1.shift())
+		joinedArr.push(arr2.shift())
+	}
+	// merge remains
+	return joinedArr.concat(arr1, text.match(tag))
+}
 
+var searches = join(text.split(tag), tokens)
 var wstacks = []
 var tstacks = []
 
-while (tokens.length) {
-	var t = tokens.pop()
-	if (isOpenTag(t)) {
-		tstacks.push(t)
+for (var i=0; i<searches.length; i++) {
+	var c = searches[i]
+	if (isTag(c)) {
+		if (isOpenTag(c)) {
+			tstacks.push(c)
+		} else {
+			// handle close tag
+
+			// pop open tag
+			var oc = tstacks.pop()
+
+			if (!oc) return throw new Error('Unmatch token', c)
+
+			var w = wstacks.pop() || ''
+			// left value
+			var lw = wstacks.pop() || ''
+			wstacks.push((lw + handler(c, w, oc)) + '')
+		}
 	} else {
-		var w = words.pop()
-		
+		wstacks.push(c)
+		tstacks.push('+')
 	}
 }
